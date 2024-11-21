eadb54d9a2a979381c3848c2718566a4416e9949
sanjiv-raghunandan <sanjivrnandan@gmail.com>
2024-11-22 00:16:33 +0530

    added procedure, function

--- db/createdb.sql
+++ db/createdb.sql
@@ -71,3 +71,29 @@ BEGIN
     SET last_updated = CURRENT_TIMESTAMP
     WHERE media_id = OLD.media_id;
 END;	
+
+CREATE FUNCTION media_exists(media_id INT) 
+RETURNS BOOLEAN
+AS $$
+BEGIN
+    RETURN EXISTS (SELECT 1 FROM media WHERE media_id = media_id);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE PROCEDURE add_user_with_defaults(
+    user_name VARCHAR(255),
+    user_email VARCHAR(319),
+    user_password VARCHAR(255),
+    user_role VARCHAR(255)
+)
+BEGIN
+    INSERT INTO user (Name, email, password, user_role, created_at)
+    VALUES (user_name, user_email, user_password, user_role, CURRENT_TIMESTAMP);
+
+    DECLARE new_user_id INT;
+    SET new_user_id = LAST_INSERT_ID();
+
+    INSERT INTO user_preferences (user_ID, category_ID, pref_app)
+    SELECT new_user_id, category_ID, 'Default App'
+    FROM media_categories;
+END;
